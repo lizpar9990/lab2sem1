96716375ca79c1c3a25136fd1a12768d9c544ebf
lizpar9990 <145855865+lizpar9990@users.noreply.github.com>
2024-04-12 23:01:17 +0300

    Update main.cpp

--- untitled6/main.cpp
+++ untitled6/main.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 int main(){
-
+    Ochki *arr=NULL;
     int n=0;
     int stop=0;
     while (stop!=1){
         int key;
-        cout<<"1-create, 2-load, 3-save, 4-search, 5-insert, 6-delete\n"<<endl;
+        cout<<"1-create, 2-load, 3-save, 4-search, 5-insert, 6-delete, 7-add, 8- print\n"<<endl;
         cin >> key;
         switch (key){
 
@@ -24,33 +24,51 @@ int main(){
                 cout << "неверное значение"<<endl;
              break;
               Ochki *arr= new Ochki[n];
-             void  _create(Ochki *arr, int n);
+             Ochkiit::_create(arr, n);
               break;
 }
 
 
         case 2:{
-            void _load (int &n);
-
+            arr = Ochkiit:: _load (n);
+            for (int i=0; i<n; i++)
+                cout << arr[i];
             break;}
 
 
-
         case 3:
-           void _save(Ochki* arr, int n);
+            arr = Ochkiit:: _load (n);
+           Ochkiit::_save(arr, n);
            break;
 
 
         case 4:
-            void _search(Ochki* arr,int n);
+            arr = Ochkiit:: _load (n);
+            Ochkiit:: _search ( arr, n);
             break;
 
         case 5:
-           void _insert(Ochki* arr, int n);
+           arr = Ochkiit:: _load (n);
+           Ochkiit::_insert(arr, n);
+           Ochkiit::_save(arr, n);
            break;
 
         case 6:
-           void _delete(Ochki* arr, int n);
+           arr = Ochkiit:: _load (n);
+           Ochkiit:: _delete(arr,  n);
+            Ochkiit::_save(arr, n);
+           break;
+        case 7:
+           arr = Ochkiit:: _load (n);
+           Ochkiit:: _add(arr,  n);
+            Ochkiit::_save(arr, n);
+           break;
+        case 8:
+           arr = Ochkiit:: _load (n);
+
+           for (int i=0; i<n; i++)
+               cout<<arr[i]._color()<<" "<<arr[i]._country()<<" "<<arr[i]._price()<<" "<<arr[i]._size()<<endl;
+
            break;
 
 

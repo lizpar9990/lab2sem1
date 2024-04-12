39bb8a397e0a0e4b39065350ce2111223e848ed6
lizpar9990 <145855865+lizpar9990@users.noreply.github.com>
2024-04-12 23:01:30 +0300

    Update Ochkiit.h

--- untitled6/Ochkiit.h
+++ untitled6/Ochkiit.h
@@ -5,14 +5,14 @@
 
 using namespace std;
 
-class Ochkiit{
-public:
+namespace Ochkiit{
 
 void _create (Ochki* arr, int n);
-void _load (int &n);
+Ochki *_load (int &n);
 void _save(Ochki* arr, int n);
 void _search(Ochki* arr,int n);
-//void _print(Ochki* arr);
+
 void _delete(Ochki* arr,int n);
 void _insert(Ochki* arr,int n);
+void _add(Ochki* arr,int n);
 };
